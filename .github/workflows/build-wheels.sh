9c14311ca567b6be195f636d9da6f2584da5e5a9
Theia Vogel <theia@vgel.me>
2022-01-11 16:53:38 -0800

    github workflow to build wheels

---
+++ .github/workflows/build-wheels.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -euxo pipefail
+
+for PYBIN in /opt/python/cp3[6789]*/bin; do
+    "${PYBIN}/pip" install maturin
+    "${PYBIN}/maturin" build -i "${PYBIN}/python" --release
+done
+
+for wheel in target/wheels/*.whl; do
+    auditwheel repair "${wheel}"
+done
