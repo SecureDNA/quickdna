7ff1bf4cfdcc0119fa801f153bf2fc730fdb05d7
Steve Wooster <s.f.m.wooster+securedna@gmail.com>
2022-12-13 21:58:05 -0800

    Add quickcheck support
    This includes Arbitrary impls for Nucleotide, NucleotideAmbiguous, Codon,
    CodonAmbiguous and DnaSequence.

---
+++ src/quickcheck.rs
@@ -0,0 +1,59 @@
+use quickcheck::{Arbitrary, Gen};
+
+use crate::{Codon, CodonAmbiguous, DnaSequence, Nucleotide, NucleotideAmbiguous, NucleotideLike};
+
+impl Arbitrary for Nucleotide {
+    fn arbitrary(g: &mut Gen) -> Self {
+        *g.choose(&[Self::A, Self::T, Self::C, Self::G])
+            .expect("Gen should be able to choose a Nucleotide")
+    }
+}
+
+impl Arbitrary for NucleotideAmbiguous {
+    fn arbitrary(g: &mut Gen) -> Self {
+        *g.choose(&[
+            Self::A,
+            Self::T,
+            Self::C,
+            Self::G,
+            Self::W,
+            Self::M,
+            Self::R,
+            Self::Y,
+            Self::S,
+            Self::K,
+            Self::B,
+            Self::V,
+            Self::D,
+            Self::H,
+            Self::N,
+        ])
+        .expect("Gen should be able to choose a NucleotideAmbiguous")
+    }
+}
+
+impl Arbitrary for Codon {
+    fn arbitrary(g: &mut Gen) -> Self {
+        Self([
+            Nucleotide::arbitrary(g),
+            Nucleotide::arbitrary(g),
+            Nucleotide::arbitrary(g),
+        ])
+    }
+}
+
+impl Arbitrary for CodonAmbiguous {
+    fn arbitrary(g: &mut Gen) -> Self {
+        Self([
+            NucleotideAmbiguous::arbitrary(g),
+            NucleotideAmbiguous::arbitrary(g),
+            NucleotideAmbiguous::arbitrary(g),
+        ])
+    }
+}
+
+impl<T: Arbitrary + NucleotideLike> Arbitrary for DnaSequence<T> {
+    fn arbitrary(g: &mut Gen) -> Self {
+        Self::new(Arbitrary::arbitrary(g))
+    }
+}
