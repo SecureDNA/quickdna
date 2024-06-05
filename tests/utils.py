29cff1ae401f6e46a211aaa7e7187e396c607fcc
Ravna <7075032+Ravna@users.noreply.github.com>
2024-06-05 02:56:22 -0400

    update copyrights

--- tests/utils.py
+++ tests/utils.py
@@ -1,4 +1,4 @@
-# Copyright 2021-2023 SecureDNA Stiftung (SecureDNA Foundation) <licensing@securedna.org>
+# Copyright 2021-2024 SecureDNA Stiftung (SecureDNA Foundation) <licensing@securedna.org>
 # SPDX-License-Identifier: MIT OR Apache-2.0
 
 import typing as ty
